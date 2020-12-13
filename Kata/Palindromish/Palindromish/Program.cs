bc6b7b7e410ad708e4b468132bf496e6bacddbc7
ken.baum <ilpadre1953@gmail.com>
2020-12-13 18:33:05 -0500

    Added kata - Palindromish

---
+++ Kata/Palindromish/Palindromish/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palindromish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
