df5ae75ff9e7ffb02ca5ac8a6f04ee10953def3e
ken.baum <ilpadre1953@gmail.com>
2020-12-15 22:01:05 -0500

    Lots of code

--- Kata/Palindromish/Palindromish-Tests/Palindromish-Tests.cs
+++ Kata/Palindromish/Palindromish-Tests/Palindromish-Tests.cs
@@ -1,4 +1,6 @@
+
 using NUnit.Framework;
+using Palindromish;
 
 namespace Palindromish_Tests
 {
@@ -10,9 +12,145 @@ namespace Palindromish_Tests
         }
 
         [Test]
-        public void Test1()
+        public void BasicSpecWordIsNotNineLong_ReturnsFalse()
         {
-            Assert.Pass();
+            string word = "blah";
+            bool isMatch = Program.IsMatchBasic(word);
+            Assert.IsFalse(isMatch);
         }
+
+
+        [Test]
+        public void BasicSpecWordIsNineLongAndDoesNotContainELI_ReturnsFalse()
+        {
+            string word = "blahblaha";
+            bool isMatch = Program.IsMatchBasic(word);
+            Assert.IsFalse(isMatch);
+        }
+
+
+        [Test]
+        public void BasicSpecWordIsNineLongAndContainsELIAndNoPalindrome_ReturnsFalse()
+        {
+            string word = "blaeliaha";
+            bool isMatch = Program.IsMatchBasic(word);
+            Assert.IsFalse(isMatch);
+        }
+
+        [Test]
+        public void BasicSpecWordIsNineLongAndContainsELIAndIsPalindrome_ReturnsTrue()
+        {
+            string word = "blaelialb";
+            bool isMatch = Program.IsMatchBasic(word);
+            Assert.IsTrue(isMatch);
+        }
+
+        [Test]
+        public void BasicSpecWordKnownSolution_ReturnsTrue()
+        {
+            string word = "timelimit";
+            bool isMatch = Program.IsMatchBasic(word);
+            Assert.IsTrue(isMatch);
+        }
+
+        [Test]
+        public void EnhancementOneEmptyword_ReturnsFalse()
+        {
+            string word = string.Empty;
+            bool isMatch = Program.IsMatchEnancement1(word);
+            Assert.IsFalse(isMatch);
+        }
+
+        [Test]
+        public void EnhancementOneWordLessThanThree_ReturnsFalse()
+        {
+            string word = "ab";
+            bool isMatch = Program.IsMatchEnancement1(word);
+            Assert.IsFalse(isMatch);
+        }
+
+        [Test]
+        public void EnhancementOneWordEqualsThreeNotELI_ReturnsFalse()
+        {
+            string word = "abc";
+            bool isMatch = Program.IsMatchEnancement1(word);
+            Assert.IsFalse(isMatch);
+        }
+
+        [Test]
+        public void EnhancementOneWordEqualsThreeIsELI_ReturnsTrue()
+        {
+            string word = "eli";
+            bool isMatch = Program.IsMatchEnancement1(word);
+            Assert.IsTrue(isMatch);
+        }
+
+        [Test]
+        public void EnhancementOneWordEqualsFour_ReturnsFalse()
+        {
+            string word = "elia";
+            bool isMatch = Program.IsMatchEnancement1(word);
+            Assert.IsFalse(isMatch);
+        }
+
+        [Test]
+        public void EnhancementOneWordLongEnoughButwordNotFound_ReturnsFalse()
+        {
+            string word = "abcba";
+            bool isMatch = Program.IsMatchEnancement1(word);
+            Assert.IsFalse(isMatch);
+        }
+
+        [Test]
+        public void EnhancementOneWordFiveContainswordNoPalindromish_ReturnsFalse()
+        {
+            string word = "aelib";
+            bool isMatch = Program.IsMatchEnancement1(word);
+            Assert.IsFalse(isMatch);
+        }
+
+        [Test]
+        public void EnhancementOneWordFiveContainswordIsPalindromish_ReturnsTrue()
+        {
+            string word = "aelia";
+            bool isMatch = Program.IsMatchEnancement1(word);
+            Assert.IsTrue(isMatch);
+        }
+
+        [Test]
+        public void EnhancementOneWordIsSolution_ReturnsTrue()
+        {
+            string word = "timelimit";
+            bool isMatch = Program.IsMatchEnancement1(word);
+            Assert.IsTrue(isMatch);
+        }
+
+        [Test]
+        public void EnhancementTwoWordIsSolution_ReturnsTrue()
+        {
+            string word = "timelimit";
+            string pattern = "eli";
+            bool isMatch = Program.IsMatchEnancement2(word, pattern);
+            Assert.IsTrue(isMatch);
+        }
+
+        [Test]
+        public void EnhancementTwoPatternLengthFiveIsPalindromish_ReturnsTrue()
+        {
+            string word = "timelimit";
+            string pattern = "melim";
+            bool isMatch = Program.IsMatchEnancement2(word, pattern);
+            Assert.IsTrue(isMatch);
+        }
+
+        [Test]
+        public void EnhancementTwoPatternLengthFiveIsNotPalindromish_ReturnsFalse()
+        {
+            string word = "tiamelimita";
+            string pattern = "melim";
+            bool isMatch = Program.IsMatchEnancement2(word, pattern);
+            Assert.IsFalse(isMatch);
+        }
+
     }
 }
--- Kata/Palindromish/Palindromish/Program.cs
+++ Kata/Palindromish/Palindromish/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using Microsoft.VisualBasic;
 
 namespace Palindromish
 {
@@ -6,16 +11,162 @@ namespace Palindromish
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<String> originalWords = ReadWords();
+
+            List<String> nineCharacterWords = originalWords.Where(w => w.Length == 9).ToList();
+
+            List<String> matchingWords = nineCharacterWords.Where(w => IsMatchBasic(w)).ToList();
+
+            string pattern = "fred";
+            List<String> matchingWordsForFred = originalWords.Where(w => IsMatchEnancement2(w, pattern)).ToList();
+
+            pattern = "api";
+            List<String> matchingWordsForApi = originalWords.Where(w => IsMatchEnancement2(w, pattern)).ToList();
+
+
         }
+
+        #region BasicRequirements
 
         public static bool IsMatchBasic(string word)
         {
             // Original requirement: 9-letter word with 'eli' in the center
+            string lcWord = word.ToLower();
             bool match = false;
             string pattern = "eli";
+            if (lcWord.Length == 9)
+            {
+                string eli = lcWord.Substring(3, 3);
+                if (eli.Equals("eli"))
+                {
+                    string prefix = lcWord.Substring(0, 3);
+                    string suffix = lcWord.Substring(6, 3);
+                    string reversedSuffix = StringReverse(suffix);
+                    if (prefix.Equals(reversedSuffix))
+                    {
+                        match = true;
+                    }
+                }
+            }
 
             return match;
+        }
+
+        #endregion
+
+
+        #region Enhancement1
+
+        public static bool IsMatchEnancement1(string word)
+        {
+            // Enhancement 1: removes 9-letter requirement, but keeps 'eli' in the center
+
+            string pattern = "eli";
+
+            if (word.Length < 3)
+            {
+                return false;
+            }
+            else if (word.Length == 3)
+            {
+                return word.Equals(pattern);
+            }
+            else if (word.Length == 4)
+            {
+                return false;
+            }
+            string lcWord = word.ToLower();
+            bool match = false;
+
+            int index = lcWord.IndexOf(pattern);
+            if (index > 0)
+            {
+                int beginPatt = index;
+                int endPatt = index + 2;
+                string prefix = lcWord.Substring(0, beginPatt);
+                string suffix = lcWord.Substring(endPatt + 1);
+                if (prefix.Length == suffix.Length)
+                {
+                    if (prefix.Equals(StringReverse(suffix)))
+                    {
+                        match = true;
+                    }
+                }
+            }   
+
+            return match;
+        }
+
+
+        #endregion
+
+        #region Enhancement2
+
+        public static bool IsMatchEnancement2(string word, string pattern)
+        {
+            // Enhancement 1: removes 9-letter requirement and hardcoded pattern.
+            int patternLen = pattern.Length;
+            if (word.Length < patternLen)
+            {
+                return false;
+            }
+            else if (word.Length == patternLen)
+            {
+                return word.Equals(pattern);
+            }
+            else if (word.Length < patternLen + 2)
+            {
+                return false;
+            }
+            string lcWord = word.ToLower();
+            bool match = false;
+
+            int index = lcWord.IndexOf(pattern);
+            if (index > 0)
+            {
+                int beginPatt = index;
+                int endPatt = index + pattern.Length-1;
+                string prefix = lcWord.Substring(0, beginPatt);
+                string suffix = lcWord.Substring(endPatt + 1);
+                if (prefix.Length == suffix.Length)
+                {
+                    if (prefix.Equals(StringReverse(suffix)))
+                    {
+                        match = true;
+                    }
+                }
+            }
+
+            return match;
+        }
+
+
+        #endregion
+        private static string StringReverse(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = s.Length; i > 0; i--)
+            {
+                sb.Append(s[i - 1]);
+            }
+
+            return sb.ToString();
+        }
+
+        private static List<string> ReadWords()
+        {
+            List<String> words = new List<String>();
+            string word;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"words.txt");
+            while ((word = file.ReadLine()) != null)
+            {
+                words.Add(word);
+            }
+
+            return words;
         }
     }
 }
