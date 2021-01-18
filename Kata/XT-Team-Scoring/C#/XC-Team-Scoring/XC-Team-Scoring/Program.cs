234e004cd7f3b9d2a0a9b7cf647d0524aecd2de1
ken.baum <ilpadre1953@gmail.com>
2021-01-17 19:51:32 -0500

    Add project and test project

---
+++ Kata/XT-Team-Scoring/C#/XC-Team-Scoring/XC-Team-Scoring/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XC_Team_Scoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
