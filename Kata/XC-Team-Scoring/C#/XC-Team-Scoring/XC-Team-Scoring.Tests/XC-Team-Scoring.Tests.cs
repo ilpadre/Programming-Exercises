4f21a793b502df889f097be9682454be936e66ab
ken.baum <ilpadre1953@gmail.com>
2021-01-24 14:35:21 -0500

    Almost done

--- Kata/XC-Team-Scoring/C#/XC-Team-Scoring/XC-Team-Scoring.Tests/XC-Team-Scoring.Tests.cs
+++ Kata/XC-Team-Scoring/C#/XC-Team-Scoring/XC-Team-Scoring.Tests/XC-Team-Scoring.Tests.cs
@@ -1,18 +1,134 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace XC_Team_Scoring.Tests
 {
     public class Tests
     {
+        private List<Runner> TestRunners { get; set; }
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Test1()
+        public void TeamCountsFourRunnersFourTeams()
         {
-            Assert.Pass();
+            TestRunners = LoadFourRunnersFromFourTeams();
+            RaceField Field = new RaceField(string.Empty, 5, 7);
+            TestRunners.ForEach(x => Field.AddRunner(x));
+            Assert.AreEqual(4, Field.TeamCounts.Count);
+            foreach (var fieldTeamCount in Field.TeamCounts)
+            {
+                Assert.AreEqual(1, fieldTeamCount.Value);
+            }
+        }
+
+        [Test]
+        public void TeamCountsFourRunnersThreeTeams()
+        {
+            TestRunners = LoadFourRunnersFromThreeTeams();
+            RaceField Field = new RaceField(string.Empty, 5, 7);
+            TestRunners.ForEach(x => Field.AddRunner(x));
+            int oneRunner = 0;
+            int twoRunners = 0;
+            foreach (var fieldTeamCount in Field.TeamCounts)
+            {
+                if (fieldTeamCount.Value == 1)
+                {
+                    oneRunner++;
+                }
+
+                if (fieldTeamCount.Value == 2)
+                {
+                    twoRunners++;
+                }
+            }
+            Assert.AreEqual(3, Field.TeamCounts.Count);
+            Assert.AreEqual(1, twoRunners);
+            Assert.AreEqual(2, oneRunner);
+        }
+
+        [Test]
+        public void TeamWithTooFewDontCountOneRunner()
+        {
+            TestRunners = LoadFiveRunnersFromThreeTeams();
+            RaceField Field = new RaceField(string.Empty, 2, 7);
+            TestRunners.ForEach(x => Field.AddRunner(x));
+            Field.AssignPoints();
+            Runner lastRunner = Field.Runners[4];
+
+            Assert.AreEqual(4, lastRunner.Points);
+            Assert.AreEqual(5, lastRunner.Place);
+
+        }
+
+
+        [Test]
+        public void TeamWithTooFewDontCountTwpRunners()
+        {
+            TestRunners = LoadSixRunnersFromFourTeams();
+            RaceField Field = new RaceField(string.Empty, 2, 7);
+            TestRunners.ForEach(x => Field.AddRunner(x));
+            Field.AssignPoints();
+            Runner lastRunner = Field.Runners[5];
+
+            Assert.AreEqual(4, lastRunner.Points);
+            Assert.AreEqual(6, lastRunner.Place);
+
+        }
+
+        private List<Runner> LoadFourRunnersFromFourTeams()
+        {
+            List<Runner> runners = new List<Runner>
+            {
+                new Runner("Noah Perry", "Central High", "17:35", 1),
+                new Runner("Jake Sandefur", "Hamilton Academy", "17:43", 2),
+                new Runner("Sam Gibbs", "Springfield", "17:44", 3),
+                new Runner("William Cook", "Gray County", "19:56", 4)
+            };
+            return runners;
+        }
+
+        private List<Runner> LoadFourRunnersFromThreeTeams()
+        {
+            List<Runner> runners = new List<Runner>
+            {
+                new Runner("Noah Perry", "Central High", "17:35", 1),
+                new Runner("Jake Sandefur", "Springfield", "17:43", 2),
+                new Runner("Sam Gibbs", "Springfield", "17:44", 3),
+                new Runner("William Cook", "Gray County", "19:56", 4)
+            };
+            return runners;
+        }
+
+        private List<Runner> LoadFiveRunnersFromThreeTeams()
+        {
+            List<Runner> runners = new List<Runner>
+            {
+                new Runner("Noah Perry", "Central High", "17:35", 1),
+                new Runner("Jake Sandefur", "Springfield", "17:43", 2),
+                new Runner("Dylan Hasler", "Central High", "19:10", 3),
+                new Runner("William Cook", "Gray County", "19:56", 4),
+                new Runner("Hunter Kohlsmith", "Gray County", "19:58", 5)
+            };
+            return runners;
+        }
+
+        private List<Runner> LoadSixRunnersFromFourTeams()
+        {
+            List<Runner> runners = new List<Runner>
+            {
+                new Runner("Noah Perry", "Central High", "17:35", 1),
+                new Runner("Jake Sandefur", "Hamilton Academy", "17:43", 2),
+                new Runner("Sam Gibbs", "Springfield", "17:44", 3),
+                new Runner("Dylan Hasler", "Central High", "19:10", 4),
+                new Runner("William Cook", "Gray County", "19:56", 5),
+                new Runner("Hunter Kohlsmith", "Gray County", "19:58", 6)
+            };
+            return runners;
         }
     }
 }
--- Kata/XC-Team-Scoring/C#/XC-Team-Scoring/XC-Team-Scoring/Program.cs
+++ Kata/XC-Team-Scoring/C#/XC-Team-Scoring/XC-Team-Scoring/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace XC_Team_Scoring
 {
@@ -9,4 +10,108 @@ namespace XC_Team_Scoring
             Console.WriteLine("Hello World!");
         }
     }
+
+    public class Runner
+    {
+        public Runner(string name, string school, string elapsedTime, int place)
+        {
+            Name = name;
+            School = school;
+            ElapsedTime = elapsedTime;
+            Place = place;
+            IncludeInTeamScore = true;
+        }
+        public  string Name { get; set; }
+        public string School { get; set; }
+        public string ElapsedTime { get; set; }
+        public bool IncludeInTeamScore { get; set; }
+        public int TeamRank { get; set; }
+        public int Place { get; set; }
+        public int Points { get; set; }
+    }
+
+    public class RaceField
+    {
+        private string ResultsFile { get; set; }
+        private int MinTeamSize { get; set; }
+        private int MaxRunnersReceivingPoints { get; set; }
+        public Dictionary<string, int> TeamCounts { get; set; }
+
+        public Dictionary<string, List<int>> TeamPlacers { get; set; }
+        public List<Runner> Runners { get; set; }
+
+        public RaceField(string resultsFile, int minTeamSize, int maxReceivingPoints)
+        {
+            ResultsFile = resultsFile;
+            MinTeamSize = minTeamSize;
+            MaxRunnersReceivingPoints = maxReceivingPoints;
+            TeamCounts = new Dictionary<string, int>();
+            TeamPlacers = new Dictionary<string, List<int>>();
+            Runners = new List<Runner>();
+        }
+
+        public void AddRunner(Runner runner)
+        {
+            string team = runner.School;
+            IncrementTeamCount(team);
+            runner.TeamRank = GetTeamCount(team);
+            Runners.Add(runner);
+
+        }
+
+        public void IncrementTeamCount(string teamName)
+        {
+            if (TeamCounts.ContainsKey(teamName))
+            {
+                TeamCounts[teamName]++;
+            }
+            else
+            {
+                TeamCounts.Add(teamName, 1);
+            }
+        }
+
+        public int GetTeamCount(string teamName)
+        {
+            return TeamCounts[teamName];
+        }
+
+        public void AssignPoints()
+        {
+            int numberToSkip = 0;
+            foreach (var runner in Runners)
+            {
+                if (GetTeamCount(runner.School) >= MinTeamSize)
+                {
+                    runner.Points = runner.Place - numberToSkip;
+                }
+                else
+                {
+                    numberToSkip += 1;
+                }
+            }
+        }
+
+        public void ComputeScore()
+        {
+
+        }
+        public void ReadRunners(string fileName)
+        {
+
+        }
+
+        public void SortRunners()
+        {
+
+        }
+
+        public void PrintResults()
+        {
+
+        }
+
+    }
+
+    
 }
