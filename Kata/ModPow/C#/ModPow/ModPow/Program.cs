a41ca91b3969d8e1e03f84b99912ac6dc6781555
ken.baum <ilpadre1953@gmail.com>
2023-01-15 16:40:40 -0500

    Initial commit

---
+++ Kata/ModPow/C#/ModPow/ModPow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModPow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
